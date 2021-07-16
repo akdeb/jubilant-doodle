0d4bb24a3675cebc79cf1ed2b342eb02ddc4d480
Akash <akad3b@gmail.com>
2021-07-15 20:53:41 -0700

    added register

---
+++ src/pages/register.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Form, Formik } from 'formik';
+import { Box, Button } from '@chakra-ui/react';
+import Wrapper from '../components/Wrapper';
+import InputField from '../components/InputField';
+import { useMutation } from 'urql';
+
+interface registerProps {
+    
+};
+
+const REGISTER_MUTATION = `
+    mutation Register($username: String!, $password: String!) {
+        register(options: { username: $username, password: $password } ) {
+          errors {
+            field
+            message
+          }
+          user {
+            id
+            username
+          }
+        }
+      }
+    `;
+
+const Register:React.FC<registerProps> = () => {
+    const [result, executeMutation] = useMutation(REGISTER_MUTATION); //urql hook
+    return (
+        <Wrapper variant="small">
+            <Formik initialValues={{ username: "", password: ""}} onSubmit={(values) => {
+                console.log(values);
+                return executeMutation(values);
+            }}>
+                {( { isSubmitting } ) => (
+                    <Form>
+                        <InputField name="username" placeholder="username" label="Username" />
+                        <Box mt={4}>
+                            <InputField name="password" placeholder="password" label="Password" type="password" />
+                        </Box>
+                        <Button mt={4} type="submit" colorScheme="teal" isLoading={isSubmitting}>Register</Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+}
+
+export default Register;
